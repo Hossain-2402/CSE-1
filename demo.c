1256d080829dd77f2630598e58bcda5af24112ef
Hossain <hossain.ie2402@gmail.com>
2024-12-04 09:53:37 +0600

    Lab 1

--- demo.c
+++ demo.c
@@ -1,5 +1,15 @@
 #include<stdio.h>
 
 int main(){
-    printf("class");
+    int myVariable = 15;
+    printf("The variable value is %d", myVariable);
+    printf("\n\n\n\n");
+
+    int firstVar = 5;
+    int secondVar = 10;
+    printf("The first variable is %d\n", firstVar);
+    printf("The Second variable is %d\n", secondVar);
+
+
+    return 12;
 }
