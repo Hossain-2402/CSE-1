0e303e830ba5ffc2dccded058054597bd8ae0488
Hossain-2402 <58787306+Hossain-2402@users.noreply.github.com>
2025-02-26 16:41:06 +0600

    Update (C)-Lab_code.c

--- (C)-Lab_code.c
+++ (C)-Lab_code.c
@@ -939,6 +939,30 @@ int main(){
 //                                          Lab-9
 
 
+#include<stdio.h>
+int main(){
+
+    int arr[3][3] = {{1,2,3},{4,5,6},{7,8,9}};
+
+    for(int i=0;i<3;i++){
+        int *a = arr[i];
+        for(int j=0;j<3;j++){
+            printf("%d ",a[j]);
+        }
+        printf("\n");
+    }
+
 
 
 }
+
+# In matrix multiplication : 
+    if result = 2x3 * 3x2
+    we need to do the multiplication k times [in each addition], where k = 3
+    The dimention of the result is 2x2
+
+# 3D:
+    int arr[2][2][2] = {
+            {{1,2},{3,4}},
+            {{5,6},{7,8}}
+        }
